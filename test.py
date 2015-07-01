e50fc5fc7f1e9b9a7daf096aede9086250307e46
venkess92 <vennkat2007@gmail.com>
2015-07-01 17:35:57 +0530

    this is test

---
+++ test.py
@@ -0,0 +1,9 @@
+print "Enter any number 1 to 100",
+num1 = raw_input()
+print "now do some operation from that",
+num2 = raw_input()
+print"the number is: %s " %(num1 , num2) 
+
+
+print "So, you enter %r ." % ( 
+	num1, num2,)
