62efec8a50ba2240b3015f9676ed5d5f5b63f9a7
venkess92 <vennkat2007@gmail.com>
2015-07-01 15:58:11 +0530

    test1

---
+++ test1.py
@@ -0,0 +1,6 @@
+print "enter your issue: "
+input1 = raw_input()
+print "how long you having this issue: "
+input2 = raw_input()
+print "your issue will be solved with %r a day " % (input1, input2)
+
